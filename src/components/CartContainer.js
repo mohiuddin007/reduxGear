fe7302efc6d8a0a7005310b49897ec4a01b94a0a
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-05-18 12:29:35 +0600

    redux gear functionality is working

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,9 +1,13 @@
 import React from "react";
 import CartItem from "./CartItem";
 import {connect} from "react-redux";
-import {CLEAR_CART} from "../Actions"
+import {CLEAR_CART, GET_TOTALS} from "../Actions"
 
 const CartContainer = ({ cart = [], total, dispatch }) => {
+   React.useEffect(() => {
+      dispatch({type: GET_TOTALS});
+   })
+
   if (cart.length === 0) {
     return (
       <section className="cart">
